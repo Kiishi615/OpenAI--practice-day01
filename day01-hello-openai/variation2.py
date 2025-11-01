dff9978ddda0c4a265e9f300f55601b3526b7c4c
Kiishi <iteoluwakiishi.aberuagba@stu.cu.edu.ng>
2025-11-01 16:41:51 +0100

    Trying gpt4

---
+++ day01-hello-openai/variation2.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import openai
+import os
+
+load_dotenv()
+client= openai.Client(api_key=os.getenv("OPENAI_API_KEY"))
+
+response=client.chat.completions.create(
+    model="gpt-4",
+    messages=[{"role": "user" , "content": "tell me a joke"}]
+
+)
+
+print(response.choices[0].message.content)
