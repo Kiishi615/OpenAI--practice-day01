d331a6e7362b1e261615b6318b9c58559f838790
Kiishi <iteoluwakiishi.aberuagba@stu.cu.edu.ng>
2025-11-03 18:18:06 +0100

    Made a working chatbot with memory

--- day02-messages/simple_loop.py
+++ day02-messages/simple_loop.py
@@ -1,6 +1,34 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+messages =[]
+client=openai.Client(api_key=os.getenv("OPENAI_API_KEY"))
+
+
 while True:
     user_input = input("You: ")
+    
     if user_input=="quit":
         print("Goodbye!")
         break
-    print("You said: ", user_input)
+    
+    messages.append({"role":"user","content":user_input})
+
+    #Model definition
+    response=client.chat.completions.create(
+        model="gpt-5-nano",
+        messages= messages
+    )
+ 
+    
+
+    reply= response.choices[0].message.content
+    print("\n AI:", reply)
+
+    messages.append({"role":"assistant","content":reply})
+
+
+
+
