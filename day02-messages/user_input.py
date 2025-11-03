a7237680a6e52eb358854e4bcf9d3e91efd7d3a8
Kiishi <iteoluwakiishi.aberuagba@stu.cu.edu.ng>
2025-11-03 17:21:32 +0100

    Add user input

---
+++ day02-messages/user_input.py
@@ -0,0 +1,22 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+user_text=input("Enter your message: ")
+print("You typed:", user_text)
+messages =[{"role":"user","content":user_text}]
+
+#API call
+client=openai.Client(api_key=os.getenv("OPENAI_API_KEY"))
+
+response=client.chat.completions.create(
+    model="gpt-5-nano",
+    messages= messages
+)
+
+messages.append({"role":"user","content":user_text})
+
+print(response.choices[0].message.content)
