c9d2ffda52e498789671333eb9f0439c2b360f36
Kiishi <iteoluwakiishi.aberuagba@stu.cu.edu.ng>
2025-11-03 16:39:47 +0100

    Send messages to API

--- day02-messages/messages_basic.py
+++ day02-messages/messages_basic.py
@@ -6,9 +6,17 @@ load_dotenv()
 
 messages = []
 messages.append({"role":"system", "content": "You are helpful"})
-messages.append({"role":"system", "content": "Hi there"})
+messages.append({"role":"user", "content": "My name is Mort"})
 
 print("message count:", len(messages))
 print("first message :", messages[0])
 print("second message :", messages[1])
 
+client =openai.Client(api_key=os.getenv("OPENAI_API_KEY"))
+response=client.chat.completions.create(
+    model="gpt-5-nano",
+    messages=messages
+
+)
+
+print(response.choices[0].message.content)
