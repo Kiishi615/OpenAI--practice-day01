70b2ef287d267e44b19b248581480fa265b463f0
Kiishi <iteoluwakiishi.aberuagba@stu.cu.edu.ng>
2025-11-04 22:00:38 +0100

    Save to file

---
+++ day03-chatbot/chatbot_v4.py
@@ -0,0 +1,28 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client=openai.Client(api_key=os.getenv("OPENAI_API_KEY"))
+messages=[{"role":"system", "content":"You are a diva"}]
+
+while True:
+    user_input=input("\nYou: ")
+    if user_input=="quit":
+        with open("chat_log.txt","w")as f:
+            f.write(str(messages))
+        break
+
+    messages.append({"role":"user", "content": user_input})
+
+    response=client.chat.completions.create(
+        model="gpt-5-nano",
+        messages=messages
+    )
+
+    reply=response.choices[0].message.content
+    print("\nAI: ", reply)
+    messages.append({"role":"assistant", "content":reply})
+    
+
