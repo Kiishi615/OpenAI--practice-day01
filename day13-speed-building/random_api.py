3416fc56a8d06d1978bcb42d83afea540a2a26e3
Kiishi <iteoluwakiishi.aberuagba@stu.cu.edu.ng>
2026-02-05 12:42:48 +0100

    Deploy on streamlit

--- day13-speed-building/random_api.py
+++ day13-speed-building/random_api.py
@@ -1,26 +1,36 @@
-import requests
+import requests, streamlit as st
 import json
-url = "https://randomuser.me/api/"
-params = {
-    "results": 5,
-    "gender" : "female",
-    "format": "json"
-}
-try: 
-    r = requests.get(url=url, params=params)
 
-    data = r.json()
+st.title("🪇 Random User Generator")
+st.write("Generate Fake users for testing")
 
-    for person in data["results"]:
-        print(f"Name:  {person['name']['title'] +'. ' + person['name']['first']+ ' ' +person['name']['last']}"),
-        print(f"Age: {person['dob']['age']}\n")
-except requests.Timeout :
-    print("Request timeout")
-except requests.HTTPError as e:
-    print(f"HTTP Error: {e.response.status_code}")
-except requests.RequestException as e:
-    print(f"Network error: {e}")
-except json.JSONDecodeError as e:
-    print(f"Invalid JSON decode")
+gender = st.selectbox("Pick a gender:", ["male", "female", "any"])
+count = st.slider("How many random people do you want: " , 1, 10, 5)
+
+if st.button("Generate User(s)"):
+    url = "https://randomuser.me/api/"
+    params = {
+        "results": count,
+        "gender" : gender if gender !="any" else " ",
+        "format": "json"
+    }
+    try: 
+        r = requests.get(url=url, params=params)
+
+        data = r.json()
+
+        for person in data["results"]:
+            name = f"{person['name']['title'] +'. ' + person['name']['first']+ ' ' +person['name']['last']}"
+            age = f"{person['dob']['age']}\n"
+
+            st.success(f"{name} - {age}")
+    except requests.Timeout :
+        print("Request timeout")
+    except requests.HTTPError as e:
+        print(f"HTTP Error: {e.response.status_code}")
+    except requests.RequestException as e:
+        print(f"Network error: {e}")
+    except json.JSONDecodeError as e:
+        print(f"Invalid JSON decode")
 
 
